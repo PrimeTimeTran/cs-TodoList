2c3a664f7d6d8765aaec3c97bc7f7f6477180a3d
Loi Tran <datloiboi@gmail.com>
2019-05-22 19:49:52 +0700

    Cleanup

--- scripts.js
+++ scripts.js
@@ -3,30 +3,32 @@ let masterTodoList = []
 const renderTodoList = () => {
   let html = ''
   masterTodoList.map((todo, idx) => {
-    const node = html += `<li id="todoItem-${idx}" style="text-decoration: ${todo.isDone ? 'line-through' : 'none'}">
-      <a href="#" onclick="toggleTodoListItem(${idx})">${todo.isDone ? 'Untoggle' : 'Toggle'}</a> ${todo.body} 
-      <a href="#" onclick="removeTodoItem(${idx})">x</a></li>
+    const node = html += `
+      <li id="todoItem-${idx}" style="text-decoration: ${todo.isDone ? 'line-through' : 'none'}">
+        <a href="#" onclick="toggleTodoListItem(${idx})" style="margin-right: 15px">${todo.isDone ? 'Untoggle' : 'Toggle'}</a> 
+        <a href="#" onclick="removeTodoItem(${idx})" style="margin-right: 15px">x</a>
+        ${todo.body} 
+      </li>
     `
     document.getElementById('todoList').innerHTML = node
   })
 }
 
 const addTodo = () => {
-  const todoItem = document.getElementById('todoItem').value
+  const body = document.getElementById('todoBody').value
   const newTodoItem = {
+    body,
+    isDone: false,
     createdAt: new Date(),
-    body: todoItem,
-    isDone: false
   }
+
   masterTodoList.push(newTodoItem)
+  document.getElementById('todoBody').value =''
   renderTodoList()
-  document.getElementById('todoItem').value =''
 }
 
 const removeTodoItem = selectedTodoIdx => {
-  // const oldTodoItem = masterTodoList[selectedTodoIdx]
   masterTodoList = masterTodoList.filter((_, idx) => idx !== selectedTodoIdx)
-  
   if (masterTodoList.length === 0) document.getElementById('todoList').innerHTML = ''
   renderTodoList()
 }
@@ -34,12 +36,10 @@ const removeTodoItem = selectedTodoIdx => {
 const toggleTodoListItem = selectedTodoIdx => {
   const oldTodoItem = masterTodoList[selectedTodoIdx]
   const newTodoItem = {
-    createdAt: oldTodoItem.createdAt,
+    ...oldTodoItem,
     isDone: !oldTodoItem.isDone,
-    body: oldTodoItem.body,
   }
 
   masterTodoList[selectedTodoIdx] = newTodoItem
   renderTodoList()
-  console.log('oldTodoItemoldTodoItemoldTodoItem', oldTodoItem)
 }
